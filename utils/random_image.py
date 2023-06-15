1eeb89513f4b6525d3fa1b2ac5b4344fe45c51e4
Andrey Plotnikov <plo-av@protonmail.com>
2023-06-16 02:14:18 +1000

    random_image - замена сайта картинок, CVE requests

--- utils/random_image.py
+++ utils/random_image.py
@@ -1,18 +1,40 @@
 from loguru import logger
 from requests import get
-from random import choices
+from random import choices, randint
 from aiogram import Bot
 from aiogram.types import Message, FSInputFile
 
 
+"""
 async def get_link(message: Message, bot: Bot) -> None:
-    """ Парсит курсы валют, показывает температуру RaspberryPi """
+    """ 'Парсит случайные изображения' """
     try:
         choice = ['river', 'nature', 'lake', 'animals', 'mountain']
         # choice = ['cats', 'town', 'cosmos', 'planet', 'sea', 'dog', 'car', 'people']
         # choice = ['sexy', 'sexual', 'beauty', 'eyes', 'lips', 'kiss', 'love', 'girl', 'woman']
         result = choices(choice)
         link = get(f'https://source.unsplash.com/random/?{result}').url
+        await bot.send_photo(message.chat.id, photo=link)
+    except Exception as error:
+        logger.error('Сервер недоступен')
+        await bot.send_message(message.chat.id, 'Сервер недоступен')
+        if str(error).find('Error code: 400'):
+            await bot.send_photo(message.chat.id,
+                                 photo=FSInputFile(path='img/vodka.jpg'))
+"""
+
+
+async def get_link(message: Message, bot: Bot) -> None:
+    """ Парсит случайные изображения """
+    try:
+        api_scenery = "https://api.scenery.cx/get_image_info/{0}"
+        link_scenery = "https://scenery.cx/images/{0}.jpg"
+        choice = randint(1, 174972)
+        api = get(api_scenery.format(choice)).text
+        if api == "null":
+            print(api)
+            return await get_link(message, bot)
+        link = link_scenery.format(choice)
         await bot.send_photo(message.chat.id, photo=link)
     except Exception as error:
         logger.error('Сервер недоступен')

6d32843d6cb181cc7ad9c4754d47b91a9e0530e8
Andrey Plotnikov <plo-av@protonmail.com>
2023-07-11 07:57:34 +1000

    (aiohttp, aiofiles) для асинх.запросов

--- utils/exchange_rate.py
+++ utils/exchange_rate.py
@@ -1,34 +1,59 @@
+import asyncio
+from aiohttp import ClientSession
+from aiofiles import open
+from json import loads
 from loguru import logger
-from requests import get
 from pathlib import Path
 from aiogram import Bot
 from aiogram.types import Message
 
 
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def read_temperature():
+    temperature = ''
+    thermal = Path('/sys/class/thermal/thermal_zone0/temp')
+    if thermal.exists():
+        async with open(thermal, 'r') as temp:
+            degree = round(int(await temp.read()) / 1000, 2)
+            temperature = f"Температура = {degree} 'C"
+    return temperature
+
+
 async def exchange(message: Message, bot: Bot) -> None:
     """ Парсит курсы валют, показывает температуру RaspberryPi """
     try:
-        binance = get('https://api.binance.com/api/v1/ticker/24hr').json()
-        price_btc, price_eth = 0, 0
-        for coin in binance:
-            if coin['symbol'] == 'BTCUSDT':
-                price_btc = int(float(coin.get('lastPrice', 0)))
-            elif coin['symbol'] == 'ETHUSDT':
-                price_eth = int(float(coin.get('lastPrice', 0)))
-        result_binance = f'BTCUSDT - {price_btc}  /  ETHUSDT - {price_eth}'
-        bank = get('https://www.cbr-xml-daily.ru/latest.js').json()
-        bank_1 = round(1 / bank["rates"]['USD'], 3)
-        bank_2 = round(1 / bank["rates"]['EUR'], 3)
-        result_bank = f'USD - {bank_1}  /  EUR - {bank_2}'
-        temperature = ''
-        thermal = Path('/sys/class/thermal/thermal_zone0/temp')
-        if thermal.exists():
-            with open(thermal, 'r') as temp:
-                temperature = round(int(temp.read()) / 1000, 2)
-                temperature = f"Температура = {temperature} 'C"
-        output = (f'<b>Центробанк РФ:   {result_bank}\nБиржа Binance:   '
-                  f'{result_binance}\n{temperature}</b>')
-        await bot.send_message(message.chat.id, output, parse_mode='HTML')
+        async with ClientSession() as session:
+
+            urls = ['https://api.binance.com/api/v1/ticker/24hr',
+                    'https://www.cbr-xml-daily.ru/latest.js']
+
+            tasks = [asyncio.create_task(fetch(session, url)) for url in urls]
+            task3 = asyncio.create_task(read_temperature())
+            result = await asyncio.gather(*tasks, task3)
+
+            binance = loads(result[0])
+            price_btc, price_eth = 0, 0
+            for coin in binance:
+                if coin['symbol'] == 'BTCUSDT':
+                    price_btc = int(float(coin.get('lastPrice', 0)))
+                elif coin['symbol'] == 'ETHUSDT':
+                    price_eth = int(float(coin.get('lastPrice', 0)))
+            result_binance = f'BTCUSDT - {price_btc}  /  ETHUSDT - {price_eth}'
+
+            bank = loads(result[1])
+            usd = 1 / bank["rates"]['USD']
+            eur = 1 / bank["rates"]['EUR']
+            result_bank = 'USD - {:.3f}  /  EUR - {:.3f}'.format(usd, eur)
+
+            output = (f'<b>Центробанк РФ:   {result_bank}\nБиржа Binance:   '
+                      f'{result_binance}\n{result[2]}</b>')
+
+            await bot.send_message(message.chat.id, output, parse_mode='HTML')
+
     except OSError:
         logger.error('Сервер недоступен')
         await bot.send_message(message.chat.id, 'Сервер недоступен')
